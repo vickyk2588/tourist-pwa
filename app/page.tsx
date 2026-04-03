d146e6a8ca7e3e0a8d2a074bd57bd8fdefd8c7be
vickyk2588 <vickyk2588@gmail.com>
2026-04-03 19:04:55 +0530

    package changes

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,12 @@ import { FetchHomeDataDocument } from '@/generated/graphql';
 import { graphqlClient } from '@/services/client';
 
 export default async function Page() {
-  const data = await graphqlClient.request(FetchHomeDataDocument);
+  let data;
+  try {
+   data = await graphqlClient.request(FetchHomeDataDocument);
+  } catch (error) {
+    console.error("Error fetching home data:", error);
+  }
 console.log("data----",data);
 
   return <Home/>;
